86b30d33693620e0878a54c37a2d89a4060b3803
barisGultekin <alibarisgultekin@gmail.com>
2024-07-03 00:44:46 +0300

    initialize project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss';
+
+export function App() {
+    return (
+        <div>
+            <h1>
+                wire
+            </h1>
+        </div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    port: 3010,
+    watchFiles: ['src/**/*'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
